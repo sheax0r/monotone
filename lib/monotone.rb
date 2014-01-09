5374b570dde19d1188bc90d493f03bfa12e1bb47
Michael Shea <mike.shea@gmail.com>
2014-01-09 09:40:32 -0500

    Switched to using File.open and yield semantics for writing to be compatible with ruby-1.8.7.

--- lib/monotone.rb
+++ lib/monotone.rb
@@ -16,14 +16,14 @@ module Monotone
       file(name) do |f|
         value = File.read(f).to_i
         value += 1
-        File.write(f, value.to_s )
+        File.open(f, 'w') {|f| f.write(value.to_s) }
         value
       end
     end
 
     def set( name , value )
       file(name) do |f|
-        File.write(f, value.to_s)
+        File.open(f, 'w') {|f| f.write(value.to_s) }
       end
       value
     end
@@ -32,12 +32,12 @@ module Monotone
 
     def file( name, &block )
       synchronize do
-        file = File.join(root_dir, name).tap do |file|
-          unless File.file?(file)
-            File.write(file, '-1')
+        file = File.join(root_dir, name).tap do |f|
+          unless File.file?(f)
+            File.open(f, 'w'){|f| f.write('-1')}
           end
         end
-        block.yield(file)
+        yield(file)
       end
     end
 
